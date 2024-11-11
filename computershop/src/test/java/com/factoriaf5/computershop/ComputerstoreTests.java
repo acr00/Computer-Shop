21d28d3b74c74c2e4c90fc4df3be4123847d0ff5
acr00 <adriancainas@gmail.com>
2024-11-11 20:24:29 +0100

    added ComputerstoreTests correction

--- computershop/src/test/java/com/factoriaf5/computershop/ComputerstoreTests.java
+++ computershop/src/test/java/com/factoriaf5/computershop/ComputerstoreTests.java
@@ -3,6 +3,7 @@ package com.factoriaf5.computershop;
 import java.util.ArrayList;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 public class ComputerstoreTests {
@@ -72,24 +73,41 @@ public class ComputerstoreTests {
         Computerstore computerStore = new Computerstore(null, null, 0);
         
         Computers computer1 = new Computers(1,"Lenovo",32, "Intel Celeron", "Windows 10",500);
+        computerStore.addComputer(computer1);
         computerStore.removeComputer(1);
         assertEquals(0, computerStore.getComputers().size());
-        assertEquals(null, computerStore.getComputers().get(0));
+    
 
     }
 
     @Test
     void TestComputerStoreFindComputerById() {
-        
+
         Computerstore computerStore = new Computerstore(null, null, 0);
         
         Computers computer1 = new Computers(1,"Apple",64, "Intel Core i7", "MacOS",1500);
         Computers computer2 = new Computers(2,"HP",32, "Intel Celeron", "Windows 10",500);
+        computerStore.addComputer(computer1);
+        computerStore.addComputer(computer2);
         computerStore.findComputerById(1);
 
         assertEquals(computer1, computerStore.getComputers().get(0));
         assertEquals(computer2, computerStore.getComputers().get(1));
         assertEquals(null, computerStore.findComputerById(3));
 
+    }
+
+    @Test
+    void TestComputerStoreListComputers(){
+        Computerstore computerStore = new Computerstore(null, null, 0);
+
+        Computers computer1 = new Computers(1, "MSI", 16, "Intel i9", "Windows 11", 1800);
+        Computers computer2 = new  Computers(2, "Alienware", 32, "AMD Ryzen 7", "Windows 10", 900);
+        computerStore.addComputer(computer1);
+        computerStore.addComputer(computer2);
+        List<Computers> list = computerStore.listComputers();
+        assertEquals(2, list.size());
+        assertTrue(list.contains(computer1));
+        assertTrue(list.contains(computer2));
     }
 }
